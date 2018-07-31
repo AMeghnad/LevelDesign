63820a260857d5a34201d0066836cf4ae49a9ddf
AMeghnad <anshumanmeghnad@gmail.com>
2018-07-31 16:07:26 +1000

    Initial commit

---
+++ Assets/Scripts/EthanController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanController : MonoBehaviour
+{
+    public Animator anim;
+    public bool isMoving;
+    [Range(-1,1)]
+    public float horizontal, vertical;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+    }
+}
